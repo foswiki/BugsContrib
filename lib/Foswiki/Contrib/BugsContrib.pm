4834991050405eef2dd27567ef8b5dbfd3dd5178
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-05 01:14:31 +0000

    Item9774: Add BugsContrib.pm to provide Version and Release strings
    git-svn-id: http://svn.foswiki.org/trunk/BugsContrib@9513 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Contrib/BugsContrib.pm
@@ -0,0 +1,16 @@
+package Foswiki::Contrib::BugsContrib;
+use strict;
+
+# This should always be $Rev: 2085 $ so that Foswiki can determine the
+# checked-in status of the plugin. It is used by the build automation tools, so
+# you should leave it alone.
+our $VERSION = '$Rev: 2085 $';
+
+# This is a free-form string you can use to "name" your own plugin version.
+# It is *not* used by the build automation tools, but is reported as part
+# of the version number in PLUGINDESCRIPTIONS.
+our $RELEASE = '4 Oct 2010';
+
+our $SHORTDESCRIPTION = 'Item Tracking system implemented as a Foswiki Application';
+
+1;
